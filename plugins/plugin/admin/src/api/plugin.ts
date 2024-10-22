efa666e1cdfa808e7e6bc2c73302553ef169972c
sovaKrya <tyan_34ee@mail.ru>
2024-10-22 16:29:57 +0300

    added styles in HomePage and added api

---
+++ plugins/plugin/admin/src/api/plugin.ts
@@ -0,0 +1,26 @@
+type Speciality = {
+  id: number;
+  name: string;
+  activity: boolean;
+  documentId: string;
+  task: string
+};
+
+export type DataSpeciality = {
+  data: Speciality[];
+  meta: {
+    pagination: {
+      page: number;
+      pageSize: number;
+      pageCount: number;
+      total: number;
+    };
+  };
+};
+
+export async function getSpecialities(): Promise<DataSpeciality> {
+  const resp = await fetch('http://localhost:1337/api/specialities');
+
+  return resp.json();
+}
+
--- plugins/plugin/admin/src/pages/HomePage.tsx
+++ plugins/plugin/admin/src/pages/HomePage.tsx
@@ -1,17 +1,65 @@
-import { SingleSelectOption } from '@strapi/design-system';
-import { Typography } from '@strapi/design-system';
-import { Main } from '@strapi/design-system';
-import { SingleSelect } from '@strapi/design-system';
+import {
+  Main,
+  Typography,
+  SingleSelectOption,
+  SingleSelect,
+  Box,
+  Button,
+} from '@strapi/design-system';
+import { useEffect, useState } from 'react';
+import { DataSpeciality, getSpecialities } from '../api/plugin';
 
 const HomePage = () => {
-  return (
-    <Main>
-      <h1 >Выбор задания</h1>
+  const [specialities, setSpecialities] = useState<DataSpeciality>();
 
-      <SingleSelect>
-        <SingleSelectOption value="front">Front</SingleSelectOption>
-        <SingleSelectOption value="front">Back</SingleSelectOption>
-      </SingleSelect>
+  function getSpecialitiesFromFetch() {
+    getSpecialities().then((res) => {
+      setSpecialities(res);
+    });
+  }
+  useEffect(() => {
+    getSpecialitiesFromFetch();
+  }, []);
+
+  return (
+    <Main
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 25,
+        paddingTop: 40,
+        paddingBottom: 40,
+        paddingLeft: 56,
+        paddingRight: 56,
+      }}
+    >
+      <Typography variant="beta" style={{ fontSize: 32, fontWeight: 400, color: '#32324D' }}>
+        Выбор задания
+      </Typography>
+
+      {!specialities ? (
+        <Typography style={{ fontWeight: 400, fontSize: 14, color: '#32324D' }}>
+          Для выбора задания необходимо добавить хотя бы одну активную специальность и хотя бы одно
+          активное задания для нее.
+        </Typography>
+      ) : (
+        <>
+          <Box style={{ display: 'flex', flexDirection: 'column', gap: 20 }}>
+            <Typography>Специальность</Typography>
+            <SingleSelect placeholder="Выберите специальность" style={{ width: 640, height: 43 }}>
+              {specialities.data.map((speciality) => {
+                if(!speciality.activity && speciality.task){
+                  return
+                }else {
+                  return <SingleSelectOption value={speciality.id} key={speciality.id}>{speciality.name}</SingleSelectOption>
+                }
+              })}
+            </SingleSelect>
+          </Box>
+
+          <Button style={{ width: 270, height: 33 }}>Сгенерировать ссылку на задание</Button>
+        </>
+      )}
     </Main>
   );
 };
